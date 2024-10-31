2e27b87597f70e81283e074018a077eef1382eef
Julian <chavez.garcia.julian2@gmail.com>
2024-10-31 13:08:00 -0600

    Añadiendo páginas y sus estilos

--- src/database/bbdd.sql
+++ src/database/bbdd.sql
@@ -11,5 +11,11 @@ create table producto(
     categoria varchar(255) not null
 );
 
-drop table producto;
+create table usuario(
+	id_usuario int primary key auto_increment not null,
+    nombre varchar(50) not null,
+    correo varchar(50) not null,
+    password varchar(50) not null,
+    permisos boolean not null
+)
 
